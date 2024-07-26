1ff4f6f43dd178082c9087356763f2ada7693a1c
yeoEun <kye020923@g.hongik.ac.kr>
2024-07-27 01:36:21 +0900

    First Commit

---
+++ models/Task.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const TaskSchema=new mongoose.Schema(
+  {
+    title:{
+      type:String,
+      required:true,
+      maxLength:30,
+      validate:{
+        validator:function(title){
+          return title.split(' ').length>1;
+        },
+        message: 'Must contain at least 2 words.',
+      },
+    },
+    discription:{
+      type: String,
+    },
+    isComplete:{
+      type:Boolean,
+      required:true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const Task=mongoose.model('Task', TaskSchema);
+
+export default Task;
